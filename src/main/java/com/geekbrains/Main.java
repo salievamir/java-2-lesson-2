159575a6abe17fd0ac3906a607c4bf76479397e2
Amir <salievamir@gmail.com>
2022-01-31 20:16:00 +0500

    Я выполнил дз с исключением, но с помощью интернета. сам своими силами не справился

---
+++ src/main/java/com/geekbrains/Main.java
@@ -0,0 +1,67 @@
+package com.geekbrains;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    //1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4, при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
+    //2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать. Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException, с детализацией в какой именно ячейке лежат неверные данные.
+    //3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException, и вывести результат расчета.
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        String[][] arr = {
+                {"1", "1", "1", "l"},
+                {"2", "2", "2", "2"},
+                {"3", "3", "3", "3"},
+                {"4", "4", "4", "4"}
+        };
+        try {
+            main.stringExceptionTest(arr);
+            main.sumOfMatrix(arr);
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stringExceptionTest(String[][] arr) throws MyArraySizeException {
+        if (arr.length != 4) throw new MyArraySizeException("Неверное количество строк");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) throw new MyArraySizeException(String.format("Неверное количество столбцов в %d-й" +
+                    " строке", i+1));
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static int sumOfMatrix(String[][] arr) throws MyArrayDataException {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException ex) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        System.out.println(sum);
+        return sum;
+    }
+}
+
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+
+class MyArrayDataException extends NumberFormatException {
+    MyArrayDataException(int row, int col) {
+        super(String.format("Допушена ошибка в %d строке, %d столбце", row + 1, col + 1));
+    }
+}
